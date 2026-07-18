9247f8b6ce24b318d2f8f232aac584abc2f8ca85
@UhLukas <119970521+UhLukas@users.noreply.github.com>
2026-07-18 22:01:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "guru-pwa-v1";
+const CACHE_NAME = "guru-pwa-v99";
 
 const FILES = [
     "./",
@@ -9,9 +9,13 @@ const FILES = [
 
 self.addEventListener("install", event => {
 
+    self.skipWaiting();
+
     event.waitUntil(
+
         caches.open(CACHE_NAME)
         .then(cache => cache.addAll(FILES))
+
     );
 
 });
@@ -20,7 +24,21 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
 
     event.waitUntil(
-        self.clients.claim()
+
+        caches.keys()
+        .then(cacheNames => {
+
+            return Promise.all(
+
+                cacheNames
+                .filter(name => name !== CACHE_NAME)
+                .map(name => caches.delete(name))
+
+            );
+
+        })
+        .then(() => self.clients.claim())
+
     );
 
 });
@@ -29,8 +47,14 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
 
     event.respondWith(
+
         caches.match(event.request)
-        .then(response => response || fetch(event.request))
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
     );
 
 });
